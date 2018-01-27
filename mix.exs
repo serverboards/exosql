cdb6cb6605cefceabcb97a1a52e66423cc29f26b
David Moreno <dmoreno@coralbits.com>
2018-01-27 18:55:16 +0100

    Updated version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExoSQL.Mixfile do
   def project do
     [
       app: :exosql,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
