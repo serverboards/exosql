74274f1dd0a240bed330c307afd0f9412ecb6dcf
David Moreno <dmoreno@coralbits.com>
2018-02-13 18:06:50 +0100

    Add support for 16 datetimes (no minutes)

--- lib/datetime.ex
+++ lib/datetime.ex
@@ -27,6 +27,10 @@ defmodule ExoSQL.DateTime do
           n = "#{n} 00:00:00Z"
           {:ok, td, 0} = DateTime.from_iso8601(n)
           td
+        16 ->
+          n = "#{n}:00Z"
+          {:ok, td, 0} = DateTime.from_iso8601(n)
+          td
         19 ->
           {:ok, td, 0} = DateTime.from_iso8601(n <> "Z")
           td
