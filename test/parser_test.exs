a71339a62cfda8b624bb81b87c8c2a573d63db81
David Moreno <dmoreno@coralbits.com>
2018-05-10 23:49:13 +0200

    Fix parser test context
    Was pretending context does is not important, and now it is.

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -5,6 +5,12 @@ defmodule ParserTest do
   doctest ExoSQL.Parser
   @moduletag :capture_log
 
+
+  @context %{
+    "A" => {ExoSQL.Csv, path: "test/data/csv/"},
+  }
+
+
   test "Lex and parse" do
     {:ok, res, 1} = :sql_lexer.string('SELECT A.products.name, A.products.stock FROM A.products WHERE (A.products.price > 0) and (a.products.stock >= 1)')
     Logger.debug("Lexed: #{inspect res}")
@@ -15,7 +21,7 @@ defmodule ParserTest do
   end
 
   test "Elixir parsing to proper struct" do
-    {:ok, res} = ExoSQL.Parser.parse("SELECT A.products.name, A.products.stock FROM A.products WHERE (A.products.price > 0) and (a.products.stock >= 1)", %{})
+    {:ok, res} = ExoSQL.Parser.parse("SELECT A.products.name, A.products.stock FROM A.products WHERE (A.products.price > 0) and (A.products.stock >= 1)", @context)
 
     Logger.debug("Parsed: #{inspect res}")
   end
